976f6483aed15cc3e7109f48c87882af67abd3af
a7medreda01 <a7med.reda01@gmail.com>
2026-06-14 15:22:24 +0300

    inti url

--- src/app/Environment/environment.prod.ts
+++ src/app/Environment/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://localhost:7233/api'
+  // apiBaseUrl: 'https://localhost:7233/api'
+  apiBaseUrl: 'https://runxtech.runasp.net/api'
 };
--- src/app/Environment/environment.ts
+++ src/app/Environment/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://localhost:7233/api'
+  // apiBaseUrl: 'https://localhost:7233/api'
+  apiBaseUrl: 'https://runxtech.runasp.net/api'
+
 };
